1035875ac3ffed7e631de5c17ff95d039fffc569
blizzwind <50780897+blizzwind@users.noreply.github.com>
2022-03-30 07:48:45 +0800

    update index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo "<title>demo</title>";
+echo "<meta charset='utf-8'>";
+echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+echo "<link rel='icon' href='/asset/icon.svg'>";
+echo "<link rel='stylesheet' href='/asset/style.min.css'>";
+
+echo "<div id='main'></div>";
+echo "<script src='/asset/script.min.js'></script>";
